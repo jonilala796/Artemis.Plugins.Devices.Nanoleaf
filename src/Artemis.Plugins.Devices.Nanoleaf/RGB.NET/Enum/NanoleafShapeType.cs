0a2a1a07860b055076fd2b359d27e3a21d618e09
jonilala796 <17726681+jonilala796@users.noreply.github.com.>
2025-03-18 20:20:27 +0100

    Add ExtControlVersion attribute to shape definitions in NanoleafShapeType

--- src/Artemis.Plugins.Devices.Nanoleaf/RGB.NET/Enum/NanoleafShapeType.cs
+++ src/Artemis.Plugins.Devices.Nanoleaf/RGB.NET/Enum/NanoleafShapeType.cs
@@ -25,37 +25,47 @@ public enum NanoleafShapeType
 
     [SideLength(100)] [Shape(Shape.Rectangle)] [ExtControlVersion(ExtControlVersion.v2)]
     ControlSquarePassive = 4,
-    [SideLength(67)] [Shape(Shape.Custom)] ShapesHexagon = 7,
 
-    [SideLength(134)] [Shape(Shape.Custom)]
+    [SideLength(67)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
+    ShapesHexagon = 7,
+
+    [SideLength(134)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
     ShapesTriangle = 8,
-    [SideLength(67)] [Shape(Shape.Custom)] ShapesMiniTriangle = 9,
+
+    [SideLength(67)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
+    ShapesMiniTriangle = 9,
     ShapesController = 12,
 
-    [SideLength(134)] [Shape(Shape.Custom)]
+    [SideLength(134)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
     ElementsHexagon = 14,
 
-    [SideLength(33.5f)] [Shape(Shape.Custom)]
+    [SideLength(33.5f)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
     ElementsHexagonCorner = 15,
-    [SideLength(11)] [Shape(Shape.Custom)] LinesConnector = 16,
 
-    [SideLength(154)] [Shape(Shape.Rectangle)]
+    [SideLength(11)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
+    LinesConnector = 16,
+
+    [SideLength(154)] [Shape(Shape.Rectangle)] [ExtControlVersion(ExtControlVersion.v2)]
     LinesLight = 17,
 
-    [SideLength(77)] [Shape(Shape.Rectangle)]
+    [SideLength(77)] [Shape(Shape.Rectangle)] [ExtControlVersion(ExtControlVersion.v2)]
     LinesLightSingleZone = 18,
-    [SideLength(11)] [Shape(Shape.Custom)] LinesControllerCap = 19,
-    [SideLength(11)] [Shape(Shape.Custom)] LinesPowerConnector = 20,
 
-    [SideLength(50)] [Shape(Shape.Rectangle)]
+    [SideLength(11)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
+    LinesControllerCap = 19,
+
+    [SideLength(11)] [Shape(Shape.Custom)] [ExtControlVersion(ExtControlVersion.v2)]
+    LinesPowerConnector = 20,
+
+    [SideLength(50)] [Shape(Shape.Rectangle)] [ExtControlVersion(ExtControlVersion.v2)]
     Lightstrip4D = 29,
 
-    [SideLength(180)] [Shape(Shape.Rectangle)]
+    [SideLength(180)] [Shape(Shape.Rectangle)] [ExtControlVersion(ExtControlVersion.v2)]
     SkylightPanel = 30,
 
-    [SideLength(180)] [Shape(Shape.Rectangle)]
+    [SideLength(180)] [Shape(Shape.Rectangle)] [ExtControlVersion(ExtControlVersion.v2)]
     SkylightControllerPrimary = 31,
 
-    [SideLength(180)] [Shape(Shape.Rectangle)]
+    [SideLength(180)] [Shape(Shape.Rectangle)] [ExtControlVersion(ExtControlVersion.v2)]
     SkylightControllerPassive = 32,
 }
